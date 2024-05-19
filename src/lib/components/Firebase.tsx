2ecf274a4060207132a83ed117c814ca245d2841
maruwtc <chriswong0621@gmail.com>
2024-05-19 18:50:21 +0800

    chore: Refactor Authentication component and remove unused code

---
+++ src/lib/components/Firebase.tsx
@@ -0,0 +1,198 @@
+import { config } from '../../../config/config';
+
+export const HandleLogin = async (email: string, password: string, toast: any) => {
+  const response = await fetch(config.api_url + '/api/v1/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  if (!response.ok) {
+    console.log(response);
+    toast({
+      title: 'Error',
+      description: 'An error occurred',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+      position: 'top',
+    });
+    return false;
+  } else {
+    const data = await response.json();
+    console.log(data);
+    if (data.error) {
+      toast({
+        title: 'Error',
+        description: data.error,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+      return false;
+    } else {
+      toast({
+        title: 'Success',
+        description: 'Login successful',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+      sessionStorage.setItem('token', data.token);
+      window.location.href = '/profile';
+      return true;
+    }
+  }
+}
+
+export const HandleLogout = () => {
+  sessionStorage.removeItem('token');
+  window.location.href = '/login';
+}
+
+export const HandleSignup = async (email: string, password: string, toast: any) => {
+  const response = await fetch(config.api_url + '/api/v1/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  if (!response.ok) {
+    toast({
+      title: 'Error',
+      description: 'An error occurred',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+      position: 'top',
+    });
+    return false;
+  } else {
+    const data = await response.json();
+    if (data.error) {
+      toast({
+        title: 'Error',
+        description: data.error,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+      return false;
+    } else {
+      toast({
+        title: 'Success',
+        description: 'Signup successful',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+      sessionStorage.setItem('email', email);
+      sessionStorage.setItem('token', data.token);
+      window.location.href = '/profile';
+      return true;
+    }
+  }
+}
+
+export const CheckAuth = async () => {
+  const response = await fetch(config.api_url + '/api/v1/checkauth', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: sessionStorage.getItem('token'),
+    }),
+  });
+  if (!response.ok) {
+    return false;
+  } else {
+    const data = await response.json();
+    if (data.error) {
+      if (data.error === 'Token expired') {
+        sessionStorage.removeItem('token');
+        console.log('Token expired');
+        window.location.href = '/login';
+      }
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+export const GetUsername = async () => {
+  const response = await fetch(config.api_url + '/api/v1/getuser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: sessionStorage.getItem('token'),
+    }),
+  });
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  } else {
+    const data = await response.json();
+    return {
+      uid: data.uid,
+      username: data.email.split('@')[0],
+      email: data.email,
+    };
+  }
+};
+
+export const DeleteAccount = async (toast: any) => {
+  const response = await fetch(config.api_url + '/api/v1/deleteaccount', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token: sessionStorage.getItem('token'),
+    }),
+  });
+  if (!response.ok) {
+    toast({
+      title: 'Error',
+      description: 'An error occurred',
+      status: 'error',
+      duration: 9000,
+      isClosable: true,
+      position: 'top',
+    });
+    return false;
+  } else {
+    const data = await response.json();
+    if (data.error) {
+      toast({
+        title: 'Error',
+        description: data.error,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+      return false;
+    } else {
+      toast({
+        title: 'Success',
+        description: 'Account deleted',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+        position: 'top',
+      });
+      sessionStorage.removeItem('token');
+      window.location.href = '/login';
+      return true;
+    }
+  }
+}
