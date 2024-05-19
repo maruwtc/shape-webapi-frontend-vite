0e163cfda518910441256f97c561ee7d8641b173
maruwtc <chriswong0621@gmail.com>
2024-05-19 18:51:32 +0800

    chore: Update Chat page to use Firebase authentication

--- src/lib/pages/chat/index.tsx
+++ src/lib/pages/chat/index.tsx
@@ -3,45 +3,52 @@ import '@sendbird/uikit-react/dist/index.css';
 import { config } from '../../../../config/config';
 import { Box } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
-import { GetUsername } from '../../components/Authentication';
-import { useNavigate } from 'react-router-dom';
+import { GetUsername, CheckAuth } from '../../components/Firebase';
+import Unauthorize from '~/lib/layout/Unauthorize';
+import { useColorMode } from '@chakra-ui/react';
 import PageLoader from '~/lib/layout/PageLoader';
 
 const Chat = () => {
     const [userid, setUserId] = useState('');
     const [username, setUserName] = useState('');
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const navigate = useNavigate(); // Initialize useNavigate
+    const [loading, setLoading] = useState(true);
+    const { colorMode } = useColorMode();
+
     useEffect(() => {
-        const fetchUsername = async () => {
+        const fetchData = async () => {
             try {
-                const { uid, username } = await GetUsername();
-                console.log(uid, username);
-                setUserId(uid);
-                setUserName(username[0].toUpperCase() + username.slice(1));
+                const isAuthenticated = await CheckAuth();
+                setIsAuthenticated(isAuthenticated);
+                if (isAuthenticated) {
+                    const { uid, username } = await GetUsername();
+                    setUserId(uid);
+                    setUserName(username[0].toUpperCase() + username.slice(1));
+                }
             } catch (error) {
                 console.error('Failed to fetch username:', error);
+            } finally {
+                setLoading(false);
             }
         };
-        const checkAuth = async () => {
-            setIsAuthenticated(true);
-            if (!isAuthenticated) {
-                navigate('/login'); // Redirect to login if not authenticated
-            }
-        }
-        fetchUsername();
-        checkAuth();
-    }, [navigate]);
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <PageLoader />
+    }
 
     if (!isAuthenticated) {
-        return <PageLoader />; // Optionally render a loading indicator or nothing while checking authentication
+        return <Unauthorize />;
     }
+
     return (
-        <Box w={'100%'} h={'80vh'}>
+        <Box w={'100%'} h={'96vh'}>
             <SendbirdApp
                 appId={config.sendbird_app_id}
                 userId={userid}
                 nickname={username}
+                theme={colorMode}
             />
         </Box>
     );
